4410a38033142c8473b09769272e452baf6a78a3
anandijain <anandj@uchicago.edu>
2020-01-29 23:24:57 -0600

    training lstm on gpu

--- lstm.py
+++ lstm.py
@@ -14,27 +14,33 @@ from torch.utils.tensorboard import SummaryWriter
 import utils
 import loaders
 import models
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
 
 BATCH_SIZE = 64
 N_LAYERS = 4
 HIDDEN_SIZE = 64
 LR = 1e-2
+
+
 dataset = loaders.WavLSTM()
 x, y = dataset[0]
 loader = DataLoader(dataset, batch_size=BATCH_SIZE)
 loss_fn = nn.MSELoss()
 writer = SummaryWriter(f'runs/{time.asctime()}')
 
-model = nn.LSTM(BATCH_SIZE*2, BATCH_SIZE*2, N_LAYERS)
+model = nn.LSTM(BATCH_SIZE*2, BATCH_SIZE*2, N_LAYERS).to(device)
 optimizer = optim.SGD(model.parameters(), lr=LR)
 
 optimizer.zero_grad()
 
-h = torch.randn(N_LAYERS, 1, BATCH_SIZE*2)
-c = torch.randn(N_LAYERS, 1, BATCH_SIZE*2)
+h = torch.randn(N_LAYERS, 1, BATCH_SIZE*2).to(device)
+c = torch.randn(N_LAYERS, 1, BATCH_SIZE*2).to(device)
 
 all_outs = []
 for i, (x, y) in enumerate(loader):
+    x = x.to(device)
+    y = y.to(device)
     print(f'x{x} y{y}')
     out, (h2, c2) = model(x.view(1, 1, -1), (h, c))
     loss = loss_fn(out, y.view(1, 1, -1))
@@ -48,4 +54,3 @@ for i, (x, y) in enumerate(loader):
 big = torch.cat(all_outs, dim=1)
 torchaudio.save('test_lstm.wav', big.view(2, -1), 44100)
 torch.save(model, 'models/lstm.pth')
-
