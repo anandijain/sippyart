7afb3f4cd11dbe5a0987844becff568b89563ad9
anandijain <anandj@uchicago.edu>
2020-03-17 17:25:17 -0500

    refactor callback, window_multiple() added for training on folder

--- SippyArt/src/bidirectional_wav.jl
+++ SippyArt/src/bidirectional_wav.jl
@@ -1,12 +1,17 @@
-using WAV, Flux
+using WAV
+using Flux
 using IterTools: ncycle
 using Plots
-
-fn = "/home/sippycups/Music/2020/81 - 3 14 20 2.wav"
-testfn = "/home/sippycups/Music/2020/81 - 3 14 20.wav"
-n = 2205
-epochs = 3 
-bs = 128
+using Glob
+dir = "/home/sippycups/Music/2020/"
+testdir = "/home/sippycups/Music/2019/"
+fns = Glob.glob("*.wav", dir)
+testfns = rand(Glob.glob("*.wav", testdir), 5)
+fn = fns[1]
+n = 1000
+hid = 50
+epochs = 1 
+bs = 64
 sr = 44100
 
 function window(fn::String)
@@ -20,12 +25,24 @@ function window(fn::String)
     return xs, ys
 end
 
-d = Flux.Data.DataLoader(window(fn)...; batchsize=bs, shuffle=false)
-td = Flux.Data.DataLoader(window(testfn)...; batchsize=bs, shuffle=false)
+function window_multiple(fns)
+    all_xs = Matrix(undef, n, 0)
+    all_ys = Matrix(undef, n, 0)
+    for f in fns
+        xs, ys = window(f)
+        all_xs = hcat(all_xs, xs)
+        all_ys = hcat(all_ys, ys)
+    end
+    return all_xs, all_ys
+end
 
-m = Chain(LSTM(n, 500),
-#  LSTM(200, 200),
-LSTM(500, n))
+xs, ys = window_multiple(fns)
+testxs, testys = window_multiple(testfns)
+
+d = Flux.Data.DataLoader(xs, ys; batchsize=bs, shuffle=false)
+td = Flux.Data.DataLoader(testxs, testys; batchsize=bs, shuffle=false)
+size(d.data[1])
+m = Chain(LSTM(n, hid), LSTM(hid, hid), LSTM(hid, n))
 
 function lxss(x, y)
     @assert size(x) == (n, bs)
@@ -38,53 +55,47 @@ end
 ps = params(m)
 opt = ADAM()
 
+function eval_loader(l::Flux.Data.DataLoader)
+    for (x, y) in l
+        @assert size(x) == (n, bs)
+        @assert size(y) == (n, bs)
+        ŷ = m(x)
+
+        loss = lxss(x, y)
+        return ŷ, loss
+    end
+end
 
 train_pred = Vector()
 test_pred = Vector()
 train_losses = Vector()
 test_losses = Vector()
+global i = 1
 cb = function ()  
-    for (x, y) in td
-        Flux.reset!(m)
-        @assert size(x) == (n, bs)
-        @assert size(y) == (n, bs)
-        test_ŷ = m(x)
+    ŷ, loss = eval_loader(d)
+    push!(train_pred, ŷ)
+    push!(train_losses, loss)
 
-        Flux.reset!(m)
-        test_loss = lxss(x, y)
-        println("test_loss:", test_loss)
-        Flux.reset!(m)
-        
-        push!(test_pred, test_ŷ)
-        push!(test_losses, test_loss)
-        break
-    end
-    for (x, y) in d
-        Flux.reset!(m)
-        ŷ = m(x[1])
-        Flux.reset!(m)
-        train_loss = lxss(x, y)
-        println("train_loss:", train_loss)
-        Flux.reset!(m)
-
-        push!(train_pred, ŷ)
-        push!(train_losses, train_loss)
-        break
-    end
+    test_ŷ, test_loss = eval_loader(td)
+    push!(test_pred, test_ŷ)
+    push!(test_losses, test_loss)
+    println("iter: ", i, ": train_loss:", loss, " test_loss:", test_loss)
+    i += 1
 end
+
 for e in 1:epochs
     println("new epoch: ", e)
-    Flux.train!(lxss, ps, td, opt, cb=cb)
+    Flux.train!(lxss, ps, d, opt, cb=cb)
 end
 
 to_write = vcat(train_pred...)
 tw = reshape(to_write, 1, :)
 size(tw)[2] / sr # seconds
 
-tw = tw[9*div(length(tw), 10):end]
+# tw = tw[div(length(tw), 1):end]
 test_write = vcat(test_pred...)
 testw = reshape(test_write, 1, :)
 testw = testw[div(length(testw), 2):end]
-wavwrite(tw, "train_out6.wav", Fs=sr)
-wavwrite(testw, "test_out6.wav", Fs=sr)
+wavwrite(tw', "train_out8.wav", Fs=sr)
+wavwrite(testw', "test_out8.wav", Fs=sr)
  
