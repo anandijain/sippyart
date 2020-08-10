2ddb292aed3fd528dcf1d26ddcf530ac51069981
anandijain <anandj@uchicago.edu>
2020-08-10 13:49:41 -0700

    remove images after composing mp4

--- SippyArt/vid.sh
+++ SippyArt/vid.sh
@@ -3,7 +3,8 @@
 
 wavtovid() {
     julia src/vid.jl $1
-    ffmpeg -r 60 -f image2 -i data/imgs/%d.png -i $1 -vcodec libx264 -crf 100 out.mp4
+    ffmpeg -r 60 -f image2 -i data/imgs/%d.png -i $1 -vcodec libx264 -crf 0 out.mp4 
+    rm -rf data/imgs
 }
 
 # wavtovid $TEST_PATH
